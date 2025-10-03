37644dae8e74ad8b73ea6fdf92a9aa8ea98365d2
Ather Salah <160007730+Ather13salah@users.noreply.github.com>
2025-10-03 15:19:02 +0300

    Update verfiy_token.py

--- app/verfiy_token.py
+++ app/verfiy_token.py
@@ -1,4 +1,4 @@
-from fastapi import Request, Response
+from fastapi import Request
 from fastapi.responses import JSONResponse
 from jose import jwt, JWTError, ExpiredSignatureError
 from starlette.middleware.base import BaseHTTPMiddleware
@@ -8,69 +8,62 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class VerifyToken(BaseHTTPMiddleware):
     def __init__(self, app):
         super().__init__(app)
         self.secret_key = os.getenv("SECRET_KEY")
         self.algorithm = os.getenv("ALGORITHM")
 
-    def check_refresh(self, refresh_token):
+    def check_refresh(self, refresh_token: str):
         try:
-            payload = jwt.decode(
-                refresh_token, self.secret_key, algorithms=[self.algorithm]
-            )
+            payload = jwt.decode(refresh_token, self.secret_key, algorithms=[self.algorithm])
             user = payload.get("sub")
-
-            # اعمل توكين جديد
-            new_token = create_access_token({"sub": user}, minutes=60)
-            return new_token
-        except (ExpiredSignatureError,JWTError):
+            return create_access_token({"sub": user}, minutes=60)
+        except (ExpiredSignatureError, JWTError):
             return None
 
     async def dispatch(self, request: Request, call_next):
         path = request.url.path
 
-        # السماح بالمسارات العامة
+        # استثناء المسارات الغير محمية
         if not path.startswith("/protected"):
             return await call_next(request)
 
         token = request.cookies.get("token")
         refresh_token = request.cookies.get("refresh_token")
 
-
-        try:
-            if token is None:
-                new_token =  self.check_refresh(refresh_token)
-                if not new_token:
-                    return JSONResponse({"invalid_token": "/login"}, status_code=401)
-                response = await call_next(request)
-                response.set_cookie(
-                    key="token",
-                    value=new_token,
-                    httponly=True,
-                    max_age=3600,
-                    samesite="none",
-                    secure=True,
-                )
-                return response
-            
-            else:
-                jwt.decode(token, self.secret_key, algorithms=[self.algorithm])
-                return await call_next(request)
-        except ExpiredSignatureError:
-            new_token =  self.check_refresh(refresh_token)
-            if not new_token:
-                return JSONResponse({"invalid_token": "/login"}, status_code=401)
-            response = await call_next(request)
+        def set_new_token(response, new_token):
             response.set_cookie(
                 key="token",
                 value=new_token,
                 httponly=True,
                 max_age=3600,
-                samesite="none",
+                samesite="None",
                 secure=True,
             )
             return response
+
+        try:
+            if token:
+                jwt.decode(token, self.secret_key, algorithms=[self.algorithm])
+                return await call_next(request)
+
+            if refresh_token:
+                new_token = self.check_refresh(refresh_token)
+                if new_token:
+                    response = await call_next(request)
+                    return set_new_token(response, new_token)
+
+            return JSONResponse({"invalid_token": "/login"}, status_code=401)
+
+        except ExpiredSignatureError:
+            if refresh_token:
+                new_token = self.check_refresh(refresh_token)
+                if new_token:
+                    response = await call_next(request)
+                    return set_new_token(response, new_token)
+
+            return JSONResponse({"invalid_token": "/login"}, status_code=401)
+
         except JWTError:
             return JSONResponse({"invalid_token": "/login"}, status_code=401)
