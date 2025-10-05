0b5a829c896fedb62e3716b33900a0ddc606b1bb
Ather Salah <160007730+Ather13salah@users.noreply.github.com>
2025-10-05 20:22:28 +0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,7 +1,9 @@
 import os
+from dotenv import load_dotenv
 import mysql.connector as sql
 from urllib.parse import urlparse
 
+load_dotenv()
 def create_connection():
     """Create connection with Railway MySQL"""
     db_url = os.getenv("DATABASE_URL")  # موجود في Railway env vars
