bc8115bb0ebdcd430323857a68bd73c11472aceb
Ather Salah <160007730+Ather13salah@users.noreply.github.com>
2025-10-05 11:45:46 +0300

    Update verfiy_token.py

--- app/verfiy_token.py
+++ app/verfiy_token.py
@@ -1,16 +1,16 @@
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from jose import jwt, JWTError, ExpiredSignatureError
 from starlette.middleware.base import BaseHTTPMiddleware
+from jose import jwt, ExpiredSignatureError, JWTError
 from app.tokens import create_access_token
 import os
+import json
 
 class VerifyToken(BaseHTTPMiddleware):
     def __init__(self, app):
         super().__init__(app)
         self.secret_key = os.getenv("SECRET_KEY")
         self.algorithm = os.getenv("ALGORITHM")
-     
 
     def decode_token(self, token):
         if not token:
@@ -27,35 +27,32 @@ class VerifyToken(BaseHTTPMiddleware):
 
         token = request.cookies.get("token")
         refresh_token = request.cookies.get("refresh_token")
-        print(f'Token:{token} and Referesh Token: {refresh_token}')# 1) Try access token
+
         payload = self.decode_token(token)
         if payload:
             request.state.user = payload.get("sub")
             return await call_next(request)
 
-        # 2) If access token missing/expired, try refresh
         payload_refresh = self.decode_token(refresh_token)
         if payload_refresh:
             user = payload_refresh.get("sub")
-            # create new access token
             new_token = create_access_token({"sub": user}, minutes=60)
-            # attach user to request so downstream handlers know the user
             request.state.user = user
 
-            # call endpoints
+            # call endpoint
             response = await call_next(request)
 
-            # set cookie on response for future requests
-            response.set_cookie(
-                key="token",
-                value=new_token,
-                httponly=True,
-                max_age=3600,
-                samesite="None",
-                secure=True,
-                path="/"
-            )
+            # حاول تعديل response إذا كان JSON
+            if response.media_type == "application/json":
+                original_body = b""
+                async for chunk in response.body_iterator:
+                    original_body += chunk
+                data = json.loads(original_body)
+                data["new_access_token"] = new_token
+                response = JSONResponse(content=data, status_code=200)
+
+            # set cookie for frontend
+            
             return response
 
-        # 3) neither token nor valid refresh -> unauthorized
         return JSONResponse({"invalid_token": "/login"}, status_code=401)
