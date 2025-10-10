bd8acd03c5b3871e511943c407975f8a3308ef41
Ather Salah <160007730+Ather13salah@users.noreply.github.com>
2025-10-10 15:03:53 +0300

    Update book_update.py

--- app/router/book_update.py
+++ app/router/book_update.py
@@ -1,14 +1,6 @@
 
 from pydantic import BaseModel
 
-class BookUpdate(BaseModel):
-    book_name: str
-    writer: str
-    publisher: str
-    category: str
-    total_pages: int
-    
-
 
 class BookData(BaseModel):
     id: str
